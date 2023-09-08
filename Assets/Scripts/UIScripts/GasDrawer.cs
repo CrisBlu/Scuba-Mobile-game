5738d3fdd8d8081e2cc28205f24bb8f6504254f2
CrisBlu <christianlav70@gmail.com>
2023-09-08 11:15:04 -0400

    Gas UI Finished; Kinda
    doesn't look good with all numbers of gas pips

--- Assets/Scripts/UIScripts/GasDrawer.cs
+++ Assets/Scripts/UIScripts/GasDrawer.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GasDrawer : MonoBehaviour
 {
     [SerializeField] GameObject gasPip;
+    [SerializeField] RectTransform gasMeter;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        DrawGasPip(4f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DrawGasPip(float gasPips)
     {
-        
+        //Get the total size of the ui element
+        Vector2 meterSize = gasMeter.sizeDelta;
+        for(int i = 0; i < gasPips; i++)
+        {
+            GameObject currentGasPip = Instantiate(gasPip, transform);
+
+            currentGasPip.transform.localScale =  new Vector2(1f, (1/gasPips));
+
+            float gasPipPosition = (meterSize.y * i/gasPips) - 70f;
+            currentGasPip.transform.localPosition = new Vector2(0f, gasPipPosition);
+        }
     }
+
 }
